9ad344ea0208abd48e8841dfe3c4af81ceb554c7
mob-sakai <sakai861104@gmail.com>
2021-01-12 06:02:12 +0900

    feat: support custom image (#54)
    * fix: configuration for rule "import/no-cycle" is invalid
    
    https://github.com/airbnb/javascript/issues/2331#issuecomment-724114465
    
    * feat: support custom image

--- src/model/input.test.js
+++ src/model/input.test.js
@@ -1,4 +1,10 @@
+import * as core from '@actions/core';
+
 import Input from './input';
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 
 describe('Input', () => {
   describe('getFromUser', () => {
@@ -8,6 +14,29 @@ describe('Input', () => {
 
     it('returns an object', () => {
       expect(typeof Input.getFromUser()).toStrictEqual('object');
+    });
+  });
+
+  describe('unityVersion', () => {
+    it('returns the default value', () => {
+      expect(Input.getFromUser().unityVersion).toStrictEqual('2019.2.11f1');
+    });
+
+    it('takes input from the users workflow', () => {
+      const mockValue = '2020.4.99f9';
+      jest.spyOn(core, 'getInput').mockReturnValue(mockValue);
+      expect(Input.getFromUser().unityVersion).toStrictEqual(mockValue);
+    });
+  });
+  describe('customImage', () => {
+    it('returns the default value', () => {
+      expect(Input.getFromUser().customImage).toStrictEqual('');
+    });
+
+    it('takes input from the users workflow', () => {
+      const mockValue = 'owner/image:2020.4.99f9';
+      jest.spyOn(core, 'getInput').mockReturnValue(mockValue);
+      expect(Input.getFromUser().customImage).toStrictEqual(mockValue);
     });
   });
 });
